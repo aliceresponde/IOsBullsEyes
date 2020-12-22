3206e14a0dd1f1709d83dce69b08176b04a72a19
Alicia Beltran <abeltran@indigoag.com>
2020-12-21 21:08:40 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Alicia Beltran on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
