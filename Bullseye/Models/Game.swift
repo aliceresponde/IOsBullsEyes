bbcfcd13470ae87fe0e165d138f2165c14602dcf
Alicia Beltran <abeltran@indigoag.com>
2021-01-10 02:18:41 -0500

    create Game Struct

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Alicia Beltran on 10/01/21.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round : Int = 0
+}
