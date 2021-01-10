db4746a7402aab0547b8603857afba2a616bc5bd
Alicia Beltran <abeltran@indigoag.com>
2021-01-10 00:36:02 -0500

    create layout, title, amount, bar, hitme btn

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                VStack{
+                    Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
+                        .bold()
+                        .kerning(2.0)
+                        .font(.footnote)
+                        .lineSpacing(4.0)
+                        .multilineTextAlignment(.center)
+                    
+                    Text("89")
+                        .kerning(-1.0)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                HStack {
+                    Text("1")   
+                        .bold()
+                        .padding()
+                    Slider(value: .constant(50), in: 1.0 ... 100.0)
+                    Text("100")
+                        .bold()
+                        .padding()
+                }
+            }
+            Button(action: {
+                print("Hello Alicia")
+            }) {
+                Text("HIT ME")
+            }
+        }
+           
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
